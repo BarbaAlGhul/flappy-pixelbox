01ef5a04d80b4f2b6a18491e46381dc376225215
BarbaAlGhul <anderson.stan5@gmail.com>
2021-03-11 11:06:09 +0100

    add player object, add handleKeyPress function

---
+++ src/scenes/game/player.js
@@ -0,0 +1,28 @@
+import constants from '../../constants'
+
+let player = {
+    x: null,
+    y: null,
+    speed: null,
+    reset: function() {
+        this.x = 35
+        this.y = constants.TILESIZE
+        this.speed = 0
+    },
+    handleKeyPress: function() {
+        // checking for button press and button release makes the character go higher if the key is pressed longer.
+        // the check on button press gives the character a vertical boost.
+        // if the character is still ascending, the check on button release limits the speed of the character.
+        // the check charY > 0 is just to prevent the player to make the character fly outside of the screen top .
+        if (this.y > 0) {
+            if (btnp.A || btnp.B) {
+                this.speed = -16
+            }
+            if (btnr.A || btnr.B) {
+                if (this.speed < -4) this.speed = -4
+            }
+        }
+    }
+}
+
+export default player
