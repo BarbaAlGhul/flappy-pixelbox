632138e720acaa3d2148a46963e8c5e3b3329a0d
BarbaAlGhul <anderson.stan5@gmail.com>
2021-03-12 12:31:39 +0100

    update gameover scene

--- src/scenes/gameover/index.js
+++ src/scenes/gameover/index.js
@@ -2,28 +2,26 @@ import constants from '../../constants'
 import scenes from '../../manageScenes'
 let TileMap = require('pixelbox/TileMap')
 
+let gameOverTile = new TileMap(10, 5)
+
 function start() {
-        this.gameOverTile = buildGameOverTile(new TileMap(10, 5))
-        this.gameOverTile.draw(constants.TILESIZE, constants.TILESIZE * 4)
-        print("GAME OVER", 4 * constants.TILESIZE, 5 * constants.TILESIZE)
-        print("PRESS R TO START", 2 * constants.TILESIZE, 6 * constants.TILESIZE)
-        print("A NEW GAME", 4 * constants.TILESIZE - 2, 7 * constants.TILESIZE)
+    // draw the game over rectangle
+    for (let x = 0; x < gameOverTile.width; x++) {
+        for (let y = 0; y < gameOverTile.height; y++) {
+            gameOverTile.set(x, y, 61)
+        }
+    }
+    gameOverTile.draw(constants.TILESIZE, constants.TILESIZE * 4)
+    print("TESTE", 8, 8)
+    print("GAME OVER", 4 * constants.TILESIZE, 5 * constants.TILESIZE)
+    print("PRESS R TO START", 2 * constants.TILESIZE, 6 * constants.TILESIZE)
+    print("A NEW GAME", 4 * constants.TILESIZE - 2, 7 * constants.TILESIZE)
 }
 
 function update() {
     if (btnp.C) {
         scenes.start('title')
     }
-}
-
-function buildGameOverTile(tileMap) {
-    // draw the game over rectangle
-    for (let x = 0; x < tileMap.width; x++) {
-        for (let y = 0; y < tileMap.height; y++) {
-            tileMap.set(x, y, 61)
-        }
-    }
-    return tileMap
 }
 
 export default {
